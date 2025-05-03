f355701b52e6161b5043f5369bc6b3c638e025dc
RahimIslam293 <110697424+RahimIslam293@users.noreply.github.com>
2025-05-03 19:47:31 -0400

    Complete project

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 import requests
 import dotenv
-
+import datetime as dt
 #CONSTANTS
 dotenv.load_dotenv()
 TOKEN = os.getenv("PIXELA_TOKEN")
@@ -33,14 +33,29 @@ unit = "hours"
 type_measured = "float"
 color = "ajisai"
 graph_headers = {"X-USER-TOKEN": TOKEN}
-graph_params = {
-    "id": GRAPH_ID,
-    "name": graph_name,
-    "unit": unit,
-    "type": type_measured,
-    "color": color
+# graph_params = {
+#     "id": GRAPH_ID,
+#     "name": graph_name,
+#     "unit": unit,
+#     "type": type_measured,
+#     "color": color
+#
+# }
+# graph_resp = requests.post(url=graph_endpoint, json=graph_params, headers=graph_headers)
+# graph_resp.raise_for_status()
+# print(graph_resp.text)
 
+pixel_posting_endpoint = f"{graph_endpoint}/{GRAPH_ID}"
+
+
+todays_date = str(dt.datetime.today().date().strftime("%Y%m%d"))
+quantity = str(1)
+
+pixel_posting_params = {
+    "date" : todays_date,
+    "quantity":quantity
 }
-graph_resp = requests.post(url=graph_endpoint, json=graph_params, headers=graph_headers)
-graph_resp.raise_for_status()
-print(graph_resp.text)
+
+post_pixel_resp = requests.post(url=pixel_posting_endpoint, headers=graph_headers,json=pixel_posting_params)
+#post_pixel_resp.raise_for_status()
+print(post_pixel_resp.text)
